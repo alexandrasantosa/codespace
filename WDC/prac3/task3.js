1c1a65797add05715f6fb1b7a675aabbffba7eea
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-03-16 02:47:29 +0000

    Wed, Mar 16, 2022, 2:47 AM +00:00

--- WDC/prac3/task3.js
+++ WDC/prac3/task3.js
@@ -3,3 +3,8 @@ var m=1;
 function mouseOver(){
      document.getElementById("mcount").innerHTML=m++;
 }
+
+function add_post(){
+    var textar=document.getElementsByTagName("textarea")[0].value;
+    
+}
