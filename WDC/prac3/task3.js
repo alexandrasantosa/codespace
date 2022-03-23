bde6c456ff2a5db8e49812828f0550da8de0ce86
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-03-23 23:46:50 +0000

    Wed, Mar 23, 2022, 11:46 PM +00:00

--- WDC/prac3/task3.js
+++ WDC/prac3/task3.js
@@ -28,7 +28,10 @@ function add_posts(){
    /* document.getElementById("posts").appendChild(d);*/
     document.getElementById("posts").appendChild(y);
 
-    const blue_opt=
+    const blue_opt=document.getElementsByName("color").checked;
+    if (blue_opt){
+        y.style.color='blue';
+    }
 
 
     /*3-8*/
