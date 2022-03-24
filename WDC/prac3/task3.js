0f3fdf6b4460e633500034bbbf8e89c5b6bdf842
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-03-24 07:28:24 +0000

    Thu, Mar 24, 2022, 7:28 AM +00:00

--- WDC/prac3/task3.js
+++ WDC/prac3/task3.js
@@ -25,7 +25,7 @@ function add_posts(){
     var col_check=document.querySelectorAll('input[type="radio"]')[0].value;
     console.log(col_check);
 
-
+    
 
 
     /* for 3-8 */ /*('input[type=radio]')*/
