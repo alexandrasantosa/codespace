449b32f20af16b4093c7eab320d514c246b996db
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-03-25 01:26:04 +0000

    Fri, Mar 25, 2022, 1:26 AM +00:00

--- WDC/prac4/routes/index.js
+++ WDC/prac4/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/last.txt',function(req,res,next){
+  res.send(date);
+  date=new Date().toString();
+});
 module.exports = router;
