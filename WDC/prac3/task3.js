25e3ff478e8aac993198cbf9f217284345806568
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-03-25 01:56:04 +0000

    Fri, Mar 25, 2022, 1:56 AM +00:00

--- WDC/prac3/task3.js
+++ WDC/prac3/task3.js
@@ -33,7 +33,7 @@ function add_posts(){
     }
 
    var num=document.querySelectorAll('input[type="number"]')[0].value;
-   for(var n=0; n<num ; num++){
+   for(var n=0; n<num ; n+){
 
     /* for 3-8 */ /*('input[type=radio]')*/
    var p1= document.getElementById("posts").appendChild(x).appendChild(d);
