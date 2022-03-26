890ed6fc5183c7a325def27b9f3388b33e633946
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-03-26 07:11:36 +0000

    Sat, Mar 26, 2022, 7:11 AM +00:00

--- WDC/prac4/routes/index.js
+++ WDC/prac4/routes/index.js
@@ -12,5 +12,18 @@ router.get('/last.txt',function(req,res,next){
   date=new Date();
 });
 
-
+var inc=0;
+router.get('/color.txt',function(req,res,next){
+  var color="";
+  inc++;
+  if(inc ==1 ){
+    color="red";
+  } else if (inc == 2 ){
+    color="yellow";
+  }else if (inc == 3){
+    color="blue";
+    inc=0;
+  }
+  res.send(color);
+})
 module.exports = router;
