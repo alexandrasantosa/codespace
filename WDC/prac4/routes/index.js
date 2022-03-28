c9d00de6211a0740887fc02ea804c7f082a4d146
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-03-28 09:59:05 +0000

    Mon, Mar 28, 2022, 9:59 AM +00:00

--- WDC/prac4/routes/index.js
+++ WDC/prac4/routes/index.js
@@ -7,48 +7,10 @@ router.get('/', function(req, res, next) {
 });
 
 var date;
-router.get('/last.txt',function(req,res){
+router.get('/last.txt',function(req,res,next){
   res.send(date);
   date=new Date();
 });
 
-var inc = 0;
-router.get('/color.html', function(req,res){
-res.send('<!DOCTYPE html>\
-     <html>\
-      <head>\
-      <title></title>\
-</head>\
-<body>\
-<h1 id="hea"></h1>\
-<script>\
-var inc='
-+ inc +
-';var h= document.getElementById("hea");\
-if(inc == 0){\
-inc++;\
-h.innerText = "red";\
-h.style.color = "red"\
-}else if(inc == 1){\
-h.innerText = "yellow";\
-h.style.color = "yellow"\
-}else if(inc == 2){\
-h.innerText = "green";\
-h.style.color = "green"\
-}else if(inc == 3){\
-h.innerText = "blue";\
-h.style.color = "blue"\
-}\
-</script>\
-</body>\
-</html>\
-');
-
-inc++;
-if(inc== 4){
-inc= 0;
-}
-
-});
 
 module.exports = router;
