be7b87073407c46a8685d47fc6020fb11d0235cc
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-03-28 08:40:16 +0000

    Mon, Mar 28, 2022, 8:40 AM +00:00

--- WDC/prac4/routes/index.js
+++ WDC/prac4/routes/index.js
@@ -22,6 +22,31 @@ router.get('/color.html',function(req,res,next){
   <body>\
   <h1 id="heading1"></h1>\
   <script>\
-  var inc='+inc+';
-  var h1)
+  var inc='+inc+'; var hea = document.getElementById("heading1");\
+  if(inc == 0){\
+  inc++;\
+  hea.innerText = "red";\
+  hea.style.color = "red"\
+  }else if(inc == 1){\
+  hea.innerText = "yellow";\
+  hea.style.color = "yellow"\
+  }else if(inc== 2){\
+  hea.innerText = "green";\
+  hea.style.color = "green"\
+  }else if(inc == 3){\
+  hea.innerText = "blue";\
+  hea.style.color = "blue"\
+  }\
+  </script>\
+  </body>\
+  </html>\
+  ');
+inc++;
+if(inc == 4){
+inc= 0;
+}
+
+});
+
+
 module.exports = router;
