a446639712a33f970fc3003c7f60b57ed8c7e16d
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-03-28 09:48:27 +0000

    Mon, Mar 28, 2022, 9:48 AM +00:00

--- WDC/prac4/routes/index.js
+++ WDC/prac4/routes/index.js
@@ -12,42 +12,43 @@ router.get('/last.txt',function(req,res,next){
   date=new Date();
 });
 
-var inc=0;
-router.get('/color.html',function(req,res,next){
-  res.send('<!DOCTYPE html>\
-  <html>\
-  <head>\
-  <title></title>\
-  </head>\
-  <body>\
-  <h1 id="heading1"></h1>\
-  <script>\
-  var inc='+inc+'; var hea = document.getElementById("heading1");\
-  if(inc == 0){\
-  inc++;\
-  hea.innerText = "red";\
-  hea.style.color = "red"\
-  }else if(inc == 1){\
-  hea.innerText = "yellow";\
-  hea.style.color = "yellow"\
-  }else if(inc== 2){\
-  hea.innerText = "green";\
-  hea.style.color = "green"\
-  }else if(inc == 3){\
-  hea.innerText = "blue";\
-  hea.style.color = "blue"\
-  }\
-  </script>\
-  </body>\
-  </html>\
-  ');
+var inc = 0;
+router.get('/color.html', function(req,res,next){
+res.send('<!DOCTYPE html>\
+     <html>\
+      <head>\
+      <title></title>\
+</head>\
+<body>\
+<h1 id="hea"></h1>\
+<script>\
+var inc='
++ inc +
+';var h= document.getElementById("hea");\
+if(inc == 0){\
+inc++;\
+h.innerText = "red";\
+h.style.color = "red"\
+}else if(inc == 1){\
+h.innerText = "yellow";\
+h.style.color = "yellow"\
+}else if(inc == 2){\
+h.innerText = "green";\
+h.style.color = "green"\
+}else if(inc == 3){\
+h.innerText = "blue";\
+h.style.color = "blue"\
+}\
+</script>\
+</body>\
+</html>\
+');
 
-  inc++;
-if(inc == 4){
+inc++;
+if(inc== 4){
 inc= 0;
 }
 
 });
-
 
 module.exports = router;
