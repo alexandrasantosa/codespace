9bad59349cc653ff1cc90a67ddc8816df9cab532
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-03-30 07:16:28 +0000

    Wed, Mar 30, 2022, 7:16 AM +00:00

---
+++ WDC/prac4/public/javascripts/task4.js
@@ -0,0 +1,12 @@
+//task4.1
+
+function time_stamps(){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange=function(){
+        if(this.readyState==4 && this.status){
+            document.getElementById("ts").innerHTML="This page was last viewed" +this. responseText;
+        }
+    };
+    xmlhttp.open("GET","/last.txt",true);
+    xmlhttp.send();
+}
