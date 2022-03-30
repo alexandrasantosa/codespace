264ee6ccc9babe7161bccd9e770b64cd126b5fad
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-03-30 07:18:04 +0000

    Wed, Mar 30, 2022, 7:18 AM +00:00

--- WDC/prac4/public/javascripts/task4.js
+++ WDC/prac4/public/javascripts/task4.js
@@ -1,6 +1,6 @@
 //task4.1
 
-function time_stamps(){
+function time_stamp(){
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange=function(){
         if(this.readyState==4 && this.status){
