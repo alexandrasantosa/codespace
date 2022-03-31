751693e2399a82dbf98a1776dc0e0475c9bfe257
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-03-31 00:05:18 +0000

    Thu, Mar 31, 2022, 12:05 AM +00:00

--- WDC/prac4/public/javascripts/task4.js
+++ WDC/prac4/public/javascripts/task4.js
@@ -64,7 +64,7 @@ function color_request(){
     var xhttp= new XMLHttpRequest();
     xhttp.onreadystatechange=function(){
         if(this.readyState ==4 && this.status == 200){
-            
+            //
 
         }
     }
