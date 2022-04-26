33b9fea9dc5149841dcfcaa43e2c08db3d702e31
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-04-26 06:09:10 +0000

    Tue, Apr 26, 2022, 6:09 AM +00:00

--- Week7/task3_vue.js
+++ Week7/task3_vue.js
@@ -6,13 +6,17 @@ var app = new Vue({
       background_value:'',
       background_color:'',
       post_content_input:'',
-      post_data:[{time:new Date(),content:"hi"}]
+      post_color_input:'',
+      post_repeat_input:1,
+      post_data:[{time:new Date(),content:"hi" , color:''}]
 
     },
 
     methods:{
         new_post:function(){
-            this.post_data.push({time:new Date(),content:this.post_content_input});
+            for(let i=0;o<this.post_repeat_input;i++){
+            this.post_data.push({time:new Date(),content:this.post_content_input ,color:this.post_color_input});
+            }
         }
     }
   });
