8eab2cd728dc2f4730d5268caff1c7b1562301c8
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-04-26 05:50:29 +0000

    Tue, Apr 26, 2022, 5:50 AM +00:00

--- Week7/task3_vue.js
+++ Week7/task3_vue.js
@@ -5,7 +5,7 @@ var app = new Vue({
       menu:false,
       background_value:'',
       background_color:'',
-      post_data[{time:new Date(),content:"Hello"}]
+      post_data:[{time:new Date(),content:"Hello"}]
 
     }
   });
