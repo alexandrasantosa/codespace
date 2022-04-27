75818ca9723984f66a84d40bbbeaeb079f01badb
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-04-27 07:08:56 +0000

    Wed, Apr 27, 2022, 7:08 AM +00:00

--- WDC/prac6/prac6.js
+++ WDC/prac6/prac6.js
@@ -47,7 +47,14 @@ var vueinst = new Vue({
     choose:'Choose ...',
     special:SPECIALS[0],
     show_ad:true,
-    darkmode:false
+    darkmode:false,
+    s_arr:[
+            { name:'Adelaide City',  address:'45 Florabunda Lane, Adelaide, 5000', counter: 0, img:'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/11_Gloddaeth_Street%2C_Llandudno_shop_front.jpg/320px-11_Gloddaeth_Street%2C_Llandudno_shop_front.jpg' },
+            { name:'Steelton South', address:'77 Weigall Avenue, Steelton, 5413',  counter: 0, img:'https://upload.wikimedia.org/wikipedia/commons/4/42/Well-shop-front.jpg' },
+            { name:'Milton',         address:'33 McGregor Street, Milton, 5880',   counter: 0, img:'https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Greggs_store_front.JPG/320px-Greggs_store_front.JPG' }
+
+    ],
+
 
     },
 
