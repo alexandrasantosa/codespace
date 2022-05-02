632f41c30187f3a013278751f7648ddaef43bef2
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-05-02 11:22:39 +0000

    Mon, May 2, 2022, 11:22 AM +00:00

--- WDC/prac7/routes/index.js
+++ WDC/prac7/routes/index.js
@@ -19,5 +19,33 @@ router.get('/brew',function(req,res,next){
 
 });
 
+//Task1-2
+var c=0;
+var prev='';
+router.post('/pass-it-on',function(req,res,next){
+  var b_print=req.body.message;
+  var a_print=req.body.submit;
+
+  if(b_print==''){
+    res.status(400).send();
+  }
+  else
+  {
+    if (c >0){
+     res.send(prev);
+     prev=b_print;
+
+    c+
+
+  }
+  else{
+    if (c==0 && b_print!=''){
+      res.send('first');
+      prev=b_print;
+      a++
+    }
+  }
+  }
+});
 
 module.exports = router;
