c8190a1653fae5ce54adb665c19a444324248ea0
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-05-03 10:08:28 +0000

    Tue, May 3, 2022, 10:08 AM +00:00

---
+++ project_draft/public/javascripts/main.js
@@ -0,0 +1,9 @@
+function open_menu(){
+    document.getElementById("main").style.display="none";
+    document.getElementById("menu").style.display="";
+ }
+
+ function back_menu(){
+    document.getElementById("main").style.display="";
+    document.getElementById("menu").style.display="none";
+ }
