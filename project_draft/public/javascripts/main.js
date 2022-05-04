df5d7e213fffdd67d458146f5bb6c9d15d433f02
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-05-04 08:27:15 +0000

    Wed, May 4, 2022, 8:27 AM +00:00

--- project_draft/public/javascripts/main.js
+++ project_draft/public/javascripts/main.js
@@ -1,9 +1,28 @@
-function open_menu(){
-    document.getElementById("men").style.display="none";
-    document.getElementById("menu").style.display="";
- }
+let slideIndex = 1;
+showSlides(slideIndex);
 
- function back_menu(){
-    document.getElementById("main").style.display="";
-    document.getElementById("men").style.display="none";
- }
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
