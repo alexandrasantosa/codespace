8dd3b817c15ee569c229b47fec5dbecfcafc5f51
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-05-07 04:12:20 +0000

    Sat, May 7, 2022, 4:12 AM +00:00

--- project_draft/public/javascripts/main.js
+++ project_draft/public/javascripts/main.js
@@ -2,22 +2,18 @@ let slide_counter = 1;
 slide_show(slide_counter);
 
 // Next/previous controls
-function plus_slides(n) {
-  slide_show(slide_counter+= n);
+function plus_slides(e) {
+  slide_show(slide_counter+= e);
 }
 
-// Thumbnail image controls
-//function current_slide(n) {
-  //slide_show(slide_counter = n);
-//}
 
-function slide_show(n) {
+function slide_show(e) {
   let i;
   let slides = document.getElementsByClassName("slide1");
 
-  if (n > slides.length) {slide_counter= 1}
-  if (n < 1) {slide_counter = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  if (e > slides.length) {slide_counter= 1}
+  if (e < 1) {slide_counter = slides.length}
+  for (e = 0; ie< slides.length; i++) {
     slides[i].style.display = "none";
   }
 
