8580d14b297e62d8b9b25b41a3ad3177117e13d5
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-05-22 12:47:03 +0000

    Sun, May 22, 2022, 12:47 PM +00:00

--- WDC/prac8/app.js
+++ WDC/prac8/app.js
@@ -7,6 +7,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var mysql=require('mysql');
+
 
 app.use(logger('dev'));
 app.use(express.json());
