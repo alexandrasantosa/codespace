419f6e43822af7b7141cb7ebb430eee43988ebd3
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-05-22 12:57:12 +0000

    Sun, May 22, 2022, 12:57 PM +00:00

--- WDC/prac8/routes/index.js
+++ WDC/prac8/routes/index.js
@@ -8,7 +8,44 @@ router.get('/', function(req, res, next) {
 
 /*Task2-1*/
 router.get('/actor',function(req,res,next){
-  req.pool.getConnection(function(err.connection))
-})
+  req.pool.getConnection(function(err.connection){
+    if(err) {
+      res.sendStatus(500);
+      return;
+  }
+  var query = `SELECT first_name, last_name FROM actor;`;
+  connection.query(query, function(err, rows, fields){
+      connection.release();  //release connection
+      if(err) {
+          res.sendStatus(500);
+          return;
+      }
+      res.json(rows);
+  });
+  });
+});
+
+router.post('/add-actor', function(req, res, next) {
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+req.pool.getConnection( function(err,connection){
+      if(err) {
+          res.sendStatus(500);
+          return;
+      }
+
+      var query = "INSERT INTO actor(first_name, last_name) VALUES(?, ?); ";
+      value = [firstName, lastName];
+      connection.query(query, value,function(err, rows, fields){
+          connection.release();  
+          if(err) {
+              res.sendStatus(500);
+              return;
+          }
+          res.json(rows);
+      });
+  });
+});
+
 
 module.exports = router;
