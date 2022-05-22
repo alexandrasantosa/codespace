049a995eb6a65c1dc6dd1eb1430e5d20a64342cf
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-05-22 12:54:16 +0000

    Sun, May 22, 2022, 12:54 PM +00:00

--- WDC/prac8/routes/index.js
+++ WDC/prac8/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/*Task2-1*/
+router.get('/actor',function(req,res,next){
+  req.pool.getConnection(function(err.connection))
+})
+
 module.exports = router;
