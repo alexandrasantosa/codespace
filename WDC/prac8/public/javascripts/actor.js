950ccc646c7783e4a767f73d69a9d136a9e98c47
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-05-22 12:59:34 +0000

    Sun, May 22, 2022, 12:59 PM +00:00

---
+++ WDC/prac8/public/javascripts/actor.js
@@ -0,0 +1,50 @@
+var actors = [];
+function get_actor(){
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+            console.log("success1");
+            let data = '';
+            if (this.readyState == 4 && this.status == 200) {
+                console.log("success");
+                actors = JSON.parse(this.responseText);
+
+                for(i=0; i<actors.length; i++)
+                {
+                    data = data +
+                    `<tr>
+                    <td>${actors[i].first_name}</td>
+                    <td>${actors[i].last_name}</td>
+                    </tr>`;
+                }
+             document.getElementById("actors").innerHTML = data;
+            }
+        };
+    xhttp.open("GET", "/users/actor", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+    
+}
+
+function add_actor() {
+
+    var firstName = document.getElementById('actor-first-name').value;
+    var lastName = document.getElementById('actor-last-name').value;
+
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+
+            if (this.readyState == 4 && this.status == 200) {
+                //console.log("success");
+            actors = JSON.parse(this.responseText);
+
+            }
+        };
+
+        var data = {"firstName": firstName, "lastName": lastName};
+
+        xhttp.open("POST", "/users/add-actor", true);
+        xhttp.setRequestHeader("Content-type", "application/json");
+        xhttp.send(JSON.stringify(data));
+}
