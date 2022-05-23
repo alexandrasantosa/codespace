44d3d6e9f0aa2577bc47e48a9daa17af4504da46
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-05-23 08:22:41 +0000

    Mon, May 23, 2022, 8:22 AM +00:00

--- WDC/prac8/public/javascripts/actor.js
+++ WDC/prac8/public/javascripts/actor.js
@@ -24,6 +24,32 @@ function get_actor(){
     xmlhttp.setRequestHeader("Content-type", "application/json");
     xmlhttp.send();
 
+}
+
+function add_actor() {
+
+    var firstName = document.getElementById('actor-first-name').value;
+    var lastName = document.getElementById('actor-last-name').value;
+    // console.log(firstName, lastName);
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            //console.log("success1");
+            // let data2 = '';
+            if (this.readyState == 4 && this.status == 200) {
+                //console.log("success");
+            actors = JSON.parse(this.responseText);
+            }
+        };
+
+        var data = {"firstName": firstName, "lastName": lastName};
+
+        xhttp.open("POST", "/users/add-actor", true);
+        xhttp.setRequestHeader("Content-type", "application/json");
+
+
+
+        xhttp.send(JSON.stringify(data));
 
 }
 
