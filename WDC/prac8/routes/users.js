d2c1de4c55559df5f4fa1e994d28bca72895a539
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-05-24 05:15:57 +0000

    Tue, May 24, 2022, 5:15 AM +00:00

--- WDC/prac8/routes/users.js
+++ WDC/prac8/routes/users.js
@@ -27,8 +27,10 @@ router.get('/actor', function(req, res, next){
 });
 
 router.post('/addactor',function(req,res,next){
+  console.log(req.body);
   var firstname=req.body.firstname;
   var lastname=req.body.lastname;
+
   req.pool.getConnection(function(error,connection){
     if(error) {
       res.sendStatus(500);
