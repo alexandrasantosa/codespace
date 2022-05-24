bfae307ad9851615c9c2d61f86bc6c0291c1b1b1
alexandrasantosa <55374103+alexandrasantosa@users.noreply.github.com>
2022-05-24 02:10:26 +0000

    Tue, May 24, 2022, 2:10 AM +00:00

--- WDC/prac8/public/javascripts/actor.js
+++ WDC/prac8/public/javascripts/actor.js
@@ -1,20 +1,19 @@
-var actors = [];
+var actor_initial = [];
 function show_actor(){
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
             let d = '';
             if (this.readyState == 4 && this.status == 200) {
-                console.log("getdatabase1");
-                actors = JSON.parse(this.responseText);
-                for(i=0; i<actors.length; i++)
+                actor_initial = JSON.parse(this.responseText);
+                for(i=0; i<actor_initial.length; i++)
                 {
                     d = d +
                     `<tr>
-                    <td>${actors[i].first_name}</td>
-                    <td>${actors[i].last_name}</td>
+                    <td>${actor_initial[i].first_name}</td>
+                    <td>${actor_initial[i].last_name}</td>
                     </tr>`;
                 }
-             document.getElementById("actors").innerHTML = d;
+             document.getElementById("actorsshow").innerHTML = d;
             }
         };
     xmlhttp.open("GET", "/users/actor", true);
